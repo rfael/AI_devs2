63c0643268a0000d7aeee4d36d8adf45d2a95f42
Rafal Ulko <rafal.ulko@gmail.com>
2024-03-22 22:21:37 +0100

    Add solution for blogger task

--- src/tasks/helloapi.rs
+++ src/tasks/helloapi.rs
@@ -4,16 +4,20 @@ use serde::Deserialize;
 use serde_json::{json, Value};
 
 #[derive(Debug, Deserialize)]
-#[allow(dead_code)]
 struct HelloApiTaskResponse {
     code: i32,
     msg: String,
     cookie: Option<String>,
 }
 
+/// Test task for learning how AI_Devs 2 task API works.
+/// The task involved retrieving messages from the API and returning the contents of the 'cookie' field in the response.
+///
+/// * `task_api_response`:
 pub(super) async fn run(task_api_response: Response) -> anyhow::Result<Value> {
     let task_response = task_api_response.json::<HelloApiTaskResponse>().await?;
     log::debug!("Task API response: {task_response:#?}");
+    log::info!("Task message: {}", task_response.msg);
 
     if task_response.code != 0 {
         bail!("Code in response is not equal 0")
