1314ff9329511f95682ff58428c4ddecf09e46d9
Rafal Ulko <rafal.ulko@gmail.com>
2024-04-23 21:34:21 +0200

    Add moderation task solution

--- src/tasks/helloapi.rs
+++ src/tasks/helloapi.rs
@@ -1,6 +1,7 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, bail};
 use reqwest::Response;
 use serde::Deserialize;
+use serde_json::{json, Value};
 
 #[derive(Debug, Deserialize)]
 #[allow(dead_code)]
@@ -10,13 +11,18 @@ struct HelloApiTaskResponse {
     cookie: Option<String>,
 }
 
-pub(super) async fn run(task_api_response: Response) -> anyhow::Result<String> {
+pub(super) async fn run(task_api_response: Response) -> anyhow::Result<Value> {
     let task_response = task_api_response.json::<HelloApiTaskResponse>().await?;
     log::debug!("Task API response: {task_response:#?}");
+
+    if task_response.code != 0 {
+        bail!("Code in response is not equal 0")
+    }
 
     let cookie = task_response
         .cookie
         .ok_or(anyhow!("API task response do not contain 'cookie' field"))?;
 
-    Ok(cookie)
+    let payload = json!({ "answer" : cookie});
+    Ok(payload)
 }
